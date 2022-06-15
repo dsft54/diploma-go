83672ed0a15e9e048e1aae1b7ca88fcfc6cffaec
dsft54 <101981695+dsft54@users.noreply.github.com>
2022-06-15 16:43:09 +0300

    empty master branch

--- cmd/settings/settings.go
+++
@@ -1,7 +0,0 @@
-package settings
-
-type Config struct {
-	ServerAddress  string `env:"RUN_ADDRESS" envDefault:"localhost:8080"`
-	DatabaseURI    string `env:"DATABASE_URI" envDefault:"postgres://postgres:example@localhost:5432"`
-	AccrualAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
-}
